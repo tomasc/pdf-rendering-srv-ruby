796232a26735efbf71a377e6ec7d15e540b8e83e
tomasc <mail@tomascelizna.com>
2021-05-09 12:13:40 +0200

    fix nesting on `pdf.margin` and `screenshot.clip`, add `cookies`

--- lib/pdf_rendering_srv/base.rb
+++ lib/pdf_rendering_srv/base.rb
@@ -6,53 +6,71 @@ module PdfRenderingSrv
       include Dry.Types()
     end
 
+    transform_keys(&:to_sym)
+
     attribute :pdf_rendering_srv_url, Types::String.optional.default(PdfRenderingSrv.configuration.pdf_rendering_srv_url.freeze)
 
     attribute :format, Types::Coercible::Symbol.enum(*%i[html pdf png jpg jpeg]).optional.default(:pdf)
-    attribute :jpeg_quality, Types::Coercible::Integer.optional.default(90)
+    attribute? :jpeg_quality, Types::Coercible::Integer.optional.default(90)
 
     # @see https://github.com/alvarcarto/url-to-pdf-api#get-apirender
 
-    attribute :emulate_screen_media, Types::Bool.optional.default(nil)
-    attribute :scroll_page, Types::Bool.optional.default(nil)
-    attribute :wait_for, Types::Coercible::String.optional.default(nil)
+    attribute? :emulate_screen_media, Types::Bool.optional
+    attribute? :scroll_page, Types::Bool.optional
+    attribute? :wait_for, Types::Coercible::String.optional
 
     attribute? :viewport do
-      attribute :width, Types::Coercible::Integer.optional.default(nil)
-      attribute :height, Types::Coercible::Integer.optional.default(nil)
-      attribute :device_scale_factor, Types::Coercible::Integer.optional.default(2)
-      attribute :is_mobile, Types::Bool.optional.default(nil)
-      attribute :has_touch, Types::Bool.optional.default(nil)
-      attribute :is_landscape, Types::Bool.optional.default(nil)
+      attribute? :width, Types::Coercible::Integer.optional
+      attribute? :height, Types::Coercible::Integer.optional
+      attribute? :device_scale_factor, Types::Coercible::Integer.optional.default(2)
+      attribute? :is_mobile, Types::Bool.optional
+      attribute? :has_touch, Types::Bool.optional
+      attribute? :is_landscape, Types::Bool.optional
+    end
+
+    attribute? :cookies, Types::Array do
+      attribute :name, Types::String
+      attribute :value, Types::String
+      attribute? :url, Types::String.optional
+      attribute? :domain, Types::String.optional
+      attribute? :path, Types::String.optional
+      attribute? :expires, Types::Coercible::Integer.optional
+      attribute? :http_only, Types::Bool.optional
+      attribute? :secure, Types::Bool.optional
+      attribute? :same_site, Types::String.optional
     end
 
     attribute? :goto do
-      attribute :wait_until, Types::Coercible::String.enum(*%w[load networkidle0 networkidle2]).optional.default(nil)
-      attribute :network_idle_timeout, Types::Integer.optional.default(nil)
+      attribute? :wait_until, Types::Coercible::String.enum(*%w[load networkidle0 networkidle2]).optional
+      attribute? :network_idle_timeout, Types::Integer.optional
     end
 
     attribute? :pdf do
-      attribute :scale, Types::Coercible::Float.optional.default(nil)
-      attribute :print_background, Types::Bool.optional.default(nil)
-      attribute :landscape, Types::Bool.optional.default(nil)
-      attribute :page_ranges, Types::Coercible::String.optional.default(nil)
-      attribute :format, Types::Coercible::String.optional.default(nil)
-      attribute :width, Types::Coercible::String.optional.default(nil)
-      attribute :height, Types::Coercible::String.optional.default(nil)
-      attribute :margin_top, Types::Coercible::String.optional.default(nil)
-      attribute :margin_right, Types::Coercible::String.optional.default(nil)
-      attribute :margin_bottom, Types::Coercible::String.optional.default(nil)
-      attribute :margin_left, Types::Coercible::String.optional.default(nil)
+      attribute? :scale, Types::Coercible::Float.optional
+      attribute? :print_background, Types::Bool.optional
+      attribute? :landscape, Types::Bool.optional
+      attribute? :page_ranges, Types::Coercible::String.optional
+      attribute? :format, Types::Coercible::String.optional
+      attribute? :width, Types::Coercible::String.optional
+      attribute? :height, Types::Coercible::String.optional
+      attribute? :margin do
+        attribute? :top, Types::Coercible::String.optional
+        attribute? :right, Types::Coercible::String.optional
+        attribute? :bottom, Types::Coercible::String.optional
+        attribute? :left, Types::Coercible::String.optional
+      end
     end
 
     attribute? :screenshot do
-      attribute :full_page, Types::Bool.optional.default(nil)
-      attribute :omit_background, Types::Bool.optional.default(nil)
-      attribute :clip_x, Types::Coercible::Integer.optional.default(nil)
-      attribute :clip_y, Types::Coercible::Integer.optional.default(nil)
-      attribute :clip_width, Types::Coercible::Integer.optional.default(nil)
-      attribute :clip_height, Types::Coercible::Integer.optional.default(nil)
-      attribute :selector, Types::Coercible::String.optional.default(nil)
+      attribute? :full_page, Types::Bool.optional
+      attribute? :omit_background, Types::Bool.optional
+      attribute? :clip do
+        attribute? :x, Types::Coercible::Integer.optional
+        attribute? :y, Types::Coercible::Integer.optional
+        attribute? :width, Types::Coercible::Integer.optional
+        attribute? :height, Types::Coercible::Integer.optional
+      end
+      attribute? :selector, Types::Coercible::String.optional
     end
 
     def self.call(*args)
